da57474a081ae8a26d0f1537e2597954408db4da
andrew grewell <andrewgrewell@gmail.com>
2018-04-19 18:10:19 -0700

    test build on circle

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Hello from CircleCI!</Text>
+        <Text>This should build</Text>
       </View>
     );
   }
